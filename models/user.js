61dbfb3eed93db47c738119d9ebc3354ff4b9b58
vellu83 <velimatti.remander@gmail.com>
2022-01-06 12:41:39 +0200

    fixed something

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
-const featureSchema = require('./feature')
+
 const uniqueValidator = require('mongoose-unique-validator')
 
 const userSchema = mongoose.Schema({
